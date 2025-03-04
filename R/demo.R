36758f9b9d182278e7cd4f6cafb6b775381658f0
pollockl-parl <pollockl@parliament.uk>
2025-03-04 11:11:55 +0000

    basic functionality test

--- R/demo.R
+++ R/demo.R
@@ -1,16 +1,40 @@
-try_register <- function(urlin) {
+#' Test APPG availability
+#'
+#' @return A boolean indicating whether we can use static parsing
+#' @export
+#'
+#' @examples
+#'  result <- try_register()
+try_register <- function() {
   tryCatch(
     {
-      return(rvest::read_html(urlin))
+      rvest::read_html(APPG_TEST_URL)
+      return(TRUE)
     },
     error = function(e){
       message(ERROR_CRAWLER)
       print(e)
+      return(FALSE)
     }
   )
 }
 
-get_appg_manual(htmlin) <- {
-  tree <- rvest::read_html(htmlin)
-  return(tree)
+get_appg_live <- function(urlin){
+  content <- rvest::read_html_live(APPG_TEST_URL) |>
+    rvest::html_element("div#content") |>
+    rvest::html_element("div#mainTextBlock")
+  return(content)
+}
+
+get_appg_static <- function(urlin) {
+  content <- rvest::read_html(urlin)
+  return(content)
+}
+
+get_appg_file <- function(filein) {
+
+}
+
+get_country_urls <- function(content) {
+  countries <- content[[1]]
 }
